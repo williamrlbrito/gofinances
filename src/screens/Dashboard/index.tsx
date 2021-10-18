0a242a3ae915dd41f595218c2c9db0e7182247ab
William Brito <williamrldb@gmail.com>
2021-10-17 22:53:40 -0400

    --Utilizando proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container, Title} from './styles'
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri:'https://avatars.githubusercontent.com/u/19253614?v=4'}} />
+            <User>
+              <UserGreeting>Ola, </UserGreeting>
+              <UserName>William</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
