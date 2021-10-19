1e227577baa4e10a96cf4cd59abe235e7375fd8c
William Brito <williamrldb@gmail.com>
2021-10-19 14:42:17 -0400

    --Deixando os cartões de transações dinâmicos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -22,29 +22,32 @@ import {
 export function Dashboard() {
   const data = [
     {
+      type: 'positive',
       title:"Desenvolvimento de site",
       amount:"R$ 12.000,00",
-      category:{
-      name: "Vendas",
-      icon: "dollar-sign"
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign"
       },
       date:"13/04/2020"
     },
     {
-      title:"Desenvolvimento de site",
-      amount:"R$ 12.000,00",
-      category:{
-      name: "Vendas",
-      icon: "dollar-sign"
+      type: 'negative',
+      title:"Hamburgueria Pizzy",
+      amount:"R$ 59,00",
+      category: {
+        name: "Alimentacao",
+        icon: "coffee"
       },
       date:"13/04/2020"
     },
     {
-      title:"Desenvolvimento de site",
-      amount:"R$ 12.000,00",
-      category:{
-      name: "Vendas",
-      icon: "dollar-sign"
+      type: 'negative',
+      title:"Aluguel do apartamento",
+      amount:"R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag"
       },
       date:"13/04/2020"
     }
