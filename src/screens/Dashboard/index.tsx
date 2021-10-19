8281a01982d8f1d84399768b0ac460cce96575cb
William Brito <williamrldb@gmail.com>
2021-10-19 09:21:29 -0400

    --Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -32,9 +32,24 @@ export function Dashboard() {
       </Header>
 
     <HighlightCards >
-      <HighlightCard />
-      <HighlightCard />
-      <HighlightCard />
+      <HighlightCard
+        type="up" 
+        title="Entradas"
+        amount="R$ 17.400,00"
+        lastTransaction="Ultima entrada dia 13 de abril"
+      />
+      <HighlightCard 
+        type="down"
+        title="Saidas"
+        amount="R$ 1.259,00"
+        lastTransaction="Ultima saida dia 03 de abril"
+      />
+      <HighlightCard 
+        type="total"
+        title="Total"
+        amount="R$ 16.141,00"
+        lastTransaction="01 a 16 de abril"
+      />
     </HighlightCards>
       
     </Container>
