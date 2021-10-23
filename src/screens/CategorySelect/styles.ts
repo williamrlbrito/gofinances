3fa22c708e665420c33225d32f05f6e60beacaa4
William Brito <williamrldb@gmail.com>
2021-10-23 11:15:46 -0400

    --Utilizando Modal

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -2,7 +2,13 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+interface CategoryProps {
+  isActive: boolean;
+}
+
+export const Container = styled(GestureHandlerRootView)`
   flex: 1;
   background-color: ${({theme}) => theme.colors.background};
 `;
@@ -23,12 +29,16 @@ export const Title = styled.Text`
   color: ${({theme}) => theme.colors.shape};
 `;
 
-export const Category = styled.View`
+export const Category = styled.TouchableOpacity<CategoryProps>`
   width: 100%;
   padding: ${RFValue(16)}px;
 
   flex-direction: row;
   align-items: center;
+
+  background-color: ${({theme, isActive}) => 
+    isActive ? theme.colors.secondary_light : theme.colors.background
+  };
 `;
 
 export const Icon = styled(Feather)`
