2653a661ff66cb7ab41f295a4c91e26f8e4ee614
William Brito <williamrldb@gmail.com>
2021-10-24 21:04:45 -0400

    --Listando os totais por categoria

--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-
 export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.colors.background};
@@ -21,3 +20,7 @@ export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
   color: ${({theme}) => theme.colors.shape};
 `;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: { flex: 1, padding: 24}
+})``;
