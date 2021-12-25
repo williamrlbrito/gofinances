f3afad215b571bd5c77e09c21030a66e87034db0
William Brito <williamrldb@gmail.com>
2021-12-24 20:22:21 -0400

    --Render nos Testes

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { Profile } from "../../screens/Profile";
+
+test('check if show correctly user input name placeholder', () => {
+  const { debug } = render(<Profile />);
+  debug();
+  // expect(toJSON()).toMatchSnapshot();
+});
