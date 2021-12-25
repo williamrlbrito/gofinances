e51f813ac0b37a5ef6bd55c97a25615f05cb9e80
William Brito <williamrldb@gmail.com>
2021-12-24 20:34:22 -0400

    --Primeiro Teste

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -4,7 +4,9 @@ import { render } from "@testing-library/react-native";
 import { Profile } from "../../screens/Profile";
 
 test('check if show correctly user input name placeholder', () => {
-  const { debug } = render(<Profile />);
-  debug();
-  // expect(toJSON()).toMatchSnapshot();
+  const { getByPlaceholderText } = render(<Profile />);
+  
+  const inputName = getByPlaceholderText('Nome');
+
+  expect(inputName).toBeTruthy();
 });
