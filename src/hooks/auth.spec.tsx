c3b48ec6f4c4dfc35d4f5122e5f2efd3c68657f4
William Brito <williamrldb@gmail.com>
2021-12-26 09:50:50 -0400

    --Mocks Personalizados

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -3,21 +3,25 @@ import { mocked } from 'jest-mock';
 import { AuthProvider, useAuth } from './auth';
 import { startAsync } from 'expo-auth-session';
 import fetchMock from 'jest-fetch-mock';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 jest.mock('expo-auth-session');
 
 fetchMock.enableMocks();
 
-const userTest = {
-  id: 'any_id',
-  email: 'john.doe@email.com',
-  name: 'John Doe',
-  photo: 'any_photo.png'
-};
-
-
 describe('Auth hook', () => {
+  beforeEach(async () => {
+    const userCollectionKey = '@gofinances:user';
+    await AsyncStorage.removeItem(userCollectionKey);
+ });
+
   it('shoulb be able to sigin with Google account existing', async () => {
+    const user = {
+      id: 'any_id',
+      email: 'john.doe@email.com',
+      name: 'John Doe',
+      photo: 'any_photo.png'
+    };
 
     const googleMocked = mocked(startAsync as any);
     googleMocked.mockReturnValueOnce({
@@ -26,8 +30,8 @@ describe('Auth hook', () => {
         access_token: 'any_token',
       }
     });
-
-    fetchMock.mockResponseOnce(JSON.stringify(userTest));
+    
+    fetchMock.mockResponseOnce(JSON.stringify(user));
 
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
@@ -36,6 +40,21 @@ describe('Auth hook', () => {
     await act(() => result.current.signInWithGoogle());
 
     expect(result.current.user.email)
-      .toBe(userTest.email);
+      .toBe(user.email);
+  });
+
+  it('user should not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel',
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty('id');
   });
 });
